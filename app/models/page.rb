6ba0616b91db0b600136b8dea62dd73a77d6a2b7
Ptotem Learning <info@ptotem.com>
2013-01-05 04:20:20 +0530

    Page List rationalization

--- app/models/page.rb
+++ app/models/page.rb
@@ -16,8 +16,8 @@ class Page < ActiveRecord::Base
   after_create :make_tasks
 
   def make_tasks
-    task_list=["Assets & Mockup", "UI Coding", "Integration"]
-    task_list_competency=["Design", "UI", "Backend"]
+    task_list=["Assets & Mockup", "User Interface", "Integration"]
+    task_list_competency=["Design", "UI Coding", "Backend Coding"]
     task_list.each_with_index do |master_task, index|
       task=Task.create!(project_id: project_id, name: "#{project.name} #{name} - #{master_task} ")
       PageTask.create!(task_id: task.id, page_id: id)
