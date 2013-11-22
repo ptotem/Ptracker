ab200bc2f061439c0aad0a0394d5d37c7d073b79
Ptotem Learning Projects <info@ptotem.com>
2013-11-22 16:46:09 +0530

    Update feature.rb

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -21,31 +21,31 @@ class Feature < ActiveRecord::Base
     case ftype
       when "Tweak"
         task_list=["Tweaking"]
-        task_list_competency=["Backend Coding"]
+        task_list_competency=["Logic"]
         task_list.each_with_index do |master_task, index|
           task=Task.create!(project_id: project_id, name: "#{project.name} #{name} - #{master_task} ")
           FeatureTask.create!(task_id: task.id, feature_id: id)
           TaskCompetence.create!(task_id: task.id, competence_id: Competence.find_by_name(task_list_competency[index]).id)
         end
       when "Standard"
-        task_list=["Data Model", "Coding", "Testing"]
-        task_list_competency=["Data Design", "Backend Coding", "Content"]
+        task_list=["Data Model", "Coding", "Content"]
+        task_list_competency=["Logic", "Logic", "Testing"]
         task_list.each_with_index do |master_task, index|
           task=Task.create!(project_id: project_id, name: "#{project.name} #{name} - #{master_task} ")
           FeatureTask.create!(task_id: task.id, feature_id: id)
           TaskCompetence.create!(task_id: task.id, competence_id: Competence.find_by_name(task_list_competency[index]).id)
         end
       when "Complex"
-        task_list=["Data Model", "Fake Data", "Algorithm", "Coding", "Testing"]
-        task_list_competency=["Data Design", "Backend Coding", "Logic", "Backend Coding", "Content"]
+        task_list=["Data Model", "Fake Data", "Algorithm", "Coding", "Content"]
+        task_list_competency=["Logic", "Testing", "Logic", "Logic", "Testing"]
         task_list.each_with_index do |master_task, index|
           task=Task.create!(project_id: project_id, name: "#{project.name} #{name} - #{master_task} ")
           FeatureTask.create!(task_id: task.id, feature_id: id)
           TaskCompetence.create!(task_id: task.id, competence_id: Competence.find_by_name(task_list_competency[index]).id)
         end
       when "Greenfield"
-        task_list=["Research", "Data Model", "Fake Data", "Algorithm", "Coding", "Testing", "Integration"]
-        task_list_competency=["Content", "Data Design", "Backend Coding", "Logic", "Backend Coding", "Content", "Backend Coding"]
+        task_list=["Research", "Data Model", "Fake Data", "Algorithm", "Coding", "Content", "Integration"]
+        task_list_competency=["Logic", "Logic", "Testing", "Logic", "Logic", "Testing", "Testing"]
         task_list.each_with_index do |master_task, index|
           task=Task.create!(project_id: project_id, name: "#{project.name} #{name} - #{master_task} ")
           FeatureTask.create!(task_id: task.id, feature_id: id)
